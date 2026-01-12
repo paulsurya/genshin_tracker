31bdb2c0511a22286b2ee77d912ccc2cd4f31bce
paulsurya <paulsurya11@gmail.com>
2026-01-12 16:42:01 +0530

    title chagne

--- streamlit_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-string = st.text_input("Enter some text:", key="user_input")
-num = st.number_input("Enter a number:", key="user_number")
-
-if string:
-    st.write(f"You entered: {string} and the number {num}")
